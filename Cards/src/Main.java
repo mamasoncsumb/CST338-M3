34f1b861655f813728b81a65d8f2878d0b18902d
mamasoncsumb <mamason@csumb.edu>
2016-09-16 11:06:07 -0700

    corrected tab formatting
    replaced tabs with 3 spaces

--- Cards/src/Main.java
+++ Cards/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Card.main(null);
-		Hand.main(null);
-	}
+   public static void main(String[] args) {
+      
+      Card.main(null);
+      Hand.main(null);
+   }
 
 }
+   
